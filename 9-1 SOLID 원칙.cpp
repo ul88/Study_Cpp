05463bd8d4ccc4824cb124bea37b7cd6dcac097f
ul88 <hkimog13@naver.com>
2024-08-06 20:33:31 +0900

    9장 끝

---
+++ 9-1 SOLID 원칙.cpp
@@ -0,0 +1,10 @@
+/*
+	SOLID 원칙 : 로버트C. 마틴이 2000년대 초반에 발표한 객체지향 설계의 다섯 가지 원칙을
+	마이클 C. 페더스가 부르기 쉽게 머리글자로 소개한 것
+	즉, 소프트웨어 유지보수와 확장성에 도움이 되는 다섯 가지 기본 원칙을 말한다.
+		Single Responsibility Principle(SRP) : 단일 책임 원칙
+		Open-Closed Principle(OCP) : 개방 폐쇄 원칙
+		Liskov Substitution Principle(LSP) : 리스코프 치환 원칙
+		Interface Segregation Principle(ISP) : 인터페이스 분리 원칙
+		Dependency Inversion Principle(DIP) : 의존성 역전 원칙
+*/
